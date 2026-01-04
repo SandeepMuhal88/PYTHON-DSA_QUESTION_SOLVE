7457d69292327f05ff0dc53ba0a128dd439a68ee
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-01-04 22:07:23 +0530

    Start Question Slove Skills

---
+++ Python_LANGUAGE/Q_01.py
@@ -0,0 +1,10 @@
+# input in python
+a= int(input("Enter a number: "))
+b= int(input("Enter a number: "))
+print("The sum of a and b is", a+b)
+
+# String input
+
+s= input("Enter a string: ")
+print("The string is", s)
+print(type(s))
