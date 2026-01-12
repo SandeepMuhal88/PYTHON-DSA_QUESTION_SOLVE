9eee0d777a4edb11a0344b6e0c472d6058177807
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-01-12 21:52:41 +0530

    Update the code

---
+++ Python_LANGUAGE/Q_03.py
@@ -0,0 +1,10 @@
+
+def isodd_even(num):
+    if num%2==0:
+        print("Numer is Even")
+    else:
+        print("Number is odd")
+
+
+n=int(input("Enter a number to check_od and even:-"))
+isodd_even(n)
