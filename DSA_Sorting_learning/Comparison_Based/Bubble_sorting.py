e61a74ae3e6aa7ecf5e982bc4599636b950efe63
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-07-04 22:15:44 +0530

    Leanr ALgorithm

--- DSA_Sorting_learning/Comparison_Based/Bubble_sorting.py
+++ DSA_Sorting_learning/Comparison_Based/Bubble_sorting.py
@@ -1,20 +1,46 @@
-def bubble_sort(arr):
+# def bubble_sort(arr):
 
+#     n = len(arr)
+
+#     for i in range(n - 1):
+
+#         for j in range(n - i - 1):
+
+#             if arr[j] > arr[j + 1]:
+
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+#     return arr
+
+
+# num=int(input("Enterr the Size of the list: "))
+
+# numbers = []
+
+# for _ in range(num):
+#     element = int(input("Enter an element: "))
+#     numbers.append(element)
+
+# bubble_sort(numbers)
+
+# print(numbers)
+
+def bubble_sort(arr):
     n = len(arr)
 
     for i in range(n - 1):
-
         for j in range(n - i - 1):
 
             if arr[j] > arr[j + 1]:
-
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
 
-
-numbers = [5, 3, 8, 4, 2]
+num = int(input("Enter the size of the list: "))
+numbers = []
+for _ in range(num):
+    element = int(input("Enter an element: "))
+    numbers.append(element)
 
 bubble_sort(numbers)
-
-print(numbers)
+print("Sorted list:", numbers)
